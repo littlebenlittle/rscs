8de8fcb7265edf6158d40bf40d18f7782de7d8cd
ben little <ben.little@benlittle.dev>
2022-11-12 18:52:49 -0700

    use arc mutex

--- src/async_alt.rs
+++ src/async_alt.rs
@@ -1,4 +1,5 @@
 use futures::channel::mpsc;
+use std::sync::{Arc, Mutex};
 
 use crate::test_fixtures::{Error, Request, Response};
 use futures::{stream, SinkExt, StreamExt};
@@ -13,19 +14,17 @@ struct Handler {
     rx: mpsc::Receiver<Request>,
 }
 
+struct Data {
+    keep_serving: bool,
+}
+
 struct Service {
     next_client_id: u8,
     handlers: Vec<Handler>,
+    data: Arc<Mutex<Data>>,
 }
 
 impl Service {
-    fn new() -> Self {
-        Self {
-            next_client_id: 0,
-            handlers: Vec::new(),
-        }
-    }
-
     fn client(&mut self) -> Result<Client, Error> {
         if self.next_client_id == u8::MAX {
             return Err(Error("exceeded max clients".to_owned()));
@@ -38,33 +37,35 @@ impl Service {
         Ok(Client { tx: ctx, rx: crx })
     }
 
-    async fn process_request(req: Request, handler: &mut Handler) {
-        match handler
-            .tx
-            .send(match req {
-                Request::A => Response::A,
-                Request::B => Response::B,
-            })
-            .await
-        {
+    async fn process_request(req: Request, handler: &mut Handler, data: &mut Data) {
+        let res = match req {
+            Request::A => Response::A,
+            Request::B => Response::B,
+        };
+        match handler.tx.send(res).await {
             Ok(()) => {}
             Err(e) => log::error!("failed to send response: {e:?}"),
         }
     }
-
-    fn keep_serving(&self) -> bool {
-        true
+    
+    async fn keep_serving(&mut self) -> bool {
+        self.data.lock().unwrap().keep_serving
     }
 
     async fn serve(&mut self) -> Result<(), Error> {
-        while self.keep_serving() {
-            stream::iter(&mut self.handlers)
-                .for_each_concurrent(None, |handler| async move {
-                    if let Some(req) = handler.rx.next().await {
-                        Self::process_request(req, handler).await;
+        while self.keep_serving().await {
+            let mut handlers = Vec::new();
+            for handler in &mut self.handlers {
+                let data_arc = self.data.clone();
+                handlers.push(Box::pin(async move {
+                    let mut data = data_arc.lock().unwrap();
+                    match handler.rx.next().await {
+                        Some(req) => Self::process_request(req, handler, &mut *data).await,
+                        None => {}
                     }
-                })
-                .await;
+                }))
+            }
+            futures::future::select_all(handlers).await;
         }
         Ok(())
     }
