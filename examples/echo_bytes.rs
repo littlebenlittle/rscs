16910096d13412eda1670e19a1d98119dddda869
ben little <ben.little@benlittle.dev>
2022-11-19 09:54:12 -0700

    byte stream echo bytes works

--- examples/echo_bytes.rs
+++ examples/echo_bytes.rs
@@ -2,7 +2,7 @@ use async_std::{
     io::{ReadExt, WriteExt},
     task::{self},
 };
-use futures::{channel::mpsc, stream, AsyncReadExt, SinkExt, StreamExt};
+use futures::{channel::mpsc, future, stream, AsyncReadExt, FutureExt, SinkExt, StreamExt};
 use std::{
     fmt::Debug,
     io::{Read, Write},
@@ -47,47 +47,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let listen_addr = listener.local_addr()?;
         let service_fut = listener
             .incoming()
-            .for_each_concurrent(Some(3), |stream| async move {
+            .take(1)
+            .for_each_concurrent(None, |stream| async move {
                 let stream = ok_or_log_err!(stream);
                 let peer_addr = ok_or_log_err!(stream.peer_addr());
                 log::debug!("new client: {}", peer_addr);
                 let (reader, mut writer) = stream.split();
-                let mut byte_stream = reader.bytes();
-                while let Some(byte) = byte_stream.next().await {
-                    let byte = ok_or_log_err!(byte);
-                    log::debug!("byte: {byte:?}");
-                    if byte == 3 {
-                        break
+                let (tx, mut rx) = mpsc::channel(32);
+                let tx = Arc::new(Mutex::new(tx));
+                let reader_fut = reader
+                    .bytes()
+                    .inspect(|byte| {
+                        let byte = ok_or_log_err!(byte);
+                        log::debug!("service got byte: {byte}");
+                    })
+                    .take_while(|byte| {
+                        future::ready(match byte {
+                            Ok(3) => false,
+                            Ok(_) => true,
+                            Err(e) => {
+                                log::error!("{e:?}");
+                                false
+                            }
+                        })
+                    })
+                    .for_each(|byte| {
+                        let tx = tx.clone();
+                        async move {
+                            let mut tx = tx.lock().unwrap();
+                            let byte = ok_or_log_err!(byte);
+                            ok_or_log_err!(tx.send(byte).await);
+                        }
+                    })
+                    .then(|_| {
+                        let tx = tx.clone();
+                        async move {
+                            let mut tx = tx.lock().unwrap();
+                            tx.close_channel();
+                        }
+                    });
+                let writer_fut = async {
+                    while let Some(byte) = rx.next().await {
+                        log::debug!("echoing byte: {byte}");
+                        ok_or_log_err!(writer.write(&[byte]).await);
                     }
-                }
-                // let (tx, mut rx) = mpsc::channel(32);
-                // let tx = Arc::new(Mutex::new(tx));
-                // let reader_fut = reader.bytes().for_each(|byte| {
-                //     log::debug!("service got a byte");
-                //     let tx = tx.clone();
-                //     async move {
-                //         let mut tx = tx.lock().unwrap();
-                //         let byte = ok_or_log_err!(byte);
-                //         log::debug!("service got byte: {byte}");
-                //         ok_or_log_err!(tx.send(byte).await);
-                //     }
-                // });
-                // let writer_fut = async {
-                //     loop {
-                //         if let Some(byte) = rx.next().await {
-                //             log::debug!("echoing byte: {byte}");
-                //             ok_or_log_err!(writer.write(&[byte]).await);
-                //         }
-                //     }
-                // };
-                // log::debug!("awaiting reader/writer futures");
-                // futures::future::join(reader_fut, writer_fut).await;
+                };
+                futures::future::join(reader_fut, writer_fut).await;
+                log::debug!("done handling client");
             });
-        let mut client = std::net::TcpStream::connect(listen_addr)?;
+        let mut client = async_std::net::TcpStream::connect(listen_addr).await?;
         let client_fut = async move {
             let (tx, mut rx) = mpsc::channel(32);
             let tx = Arc::new(Mutex::new(tx));
-            let bytes = [1, 2, 3];
+            let bytes = [41, 42, 43, 3];
             let num_bytes = bytes.len();
             let send_fut = stream::iter(bytes).for_each(|byte| {
                 let tx = tx.clone();
@@ -100,15 +112,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 for _ in 1..=num_bytes {
                     if let Some(byte) = rx.next().await {
                         log::debug!("sending byte: {byte}");
-                        ok_or_log_err!( client.write(&[byte]));
+                        ok_or_log_err!(client.write(&[byte]).await);
                     }
                 }
             };
             futures::future::join(send_fut, recv_fut).await;
             assert_eq!(
-                client.bytes().map(|b| b.unwrap()).collect::<Vec<u8>>(),
-                bytes
+                client
+                    .bytes()
+                    .inspect(|byte| {
+                        let byte = ok_or_log_err!(byte);
+                        log::debug!("client got byte: {byte}");
+                    })
+                    .take_while(|byte| {
+                        future::ready(match byte {
+                            Ok(3) => false,
+                            Ok(_) => true,
+                            Err(e) => {
+                                log::error!("{e:?}");
+                                false
+                            }
+                        })
+                    })
+                    .map(|b| b.unwrap())
+                    .collect::<Vec<u8>>()
+                    .await,
+                bytes[0..3]
             );
+            log::debug!("client done");
         };
         futures::future::join(service_fut, client_fut).await;
         Result::<(), Error>::Ok(())
