b0effdd386ea18d50d3f6d60c2f6be17fc38a261
ben little <ben.little@benlittle.dev>
2022-11-19 12:22:05 -0700

    get byte streams working on echo example

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,26 +1,15 @@
 use async_std::{
     io::{ReadExt, WriteExt},
-    task::{self},
+    task,
 };
-use futures::{channel::mpsc, stream, AsyncReadExt, SinkExt, StreamExt};
+use futures::{channel::mpsc, future, stream, AsyncReadExt, FutureExt, SinkExt, StreamExt};
 use std::{
     fmt::Debug,
-    io::Write,
-    net::{SocketAddr, TcpStream},
+    net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
 use rscs::{ParseStatus, ParseWith, Parser};
-
-fn take_ok_or_log_err<T, E: Debug>(r: Result<T, E>) -> Option<T> {
-    match r {
-        Ok(b) => Some(b),
-        Err(e) => {
-            log::error!("{e:?}");
-            None
-        }
-    }
-}
 
 #[derive(Debug, PartialEq)]
 enum Message {
@@ -28,11 +17,17 @@ enum Message {
     Echo(String),
 }
 
-struct MessageEncoder {}
+struct MessageEncoder {
+    more: Option<Vec<u8>>,
+    more_index: usize,
+}
 
 impl MessageEncoder {
     fn new() -> Self {
-        Self {}
+        Self {
+            more: Some(Vec::new()),
+            more_index: 0,
+        }
     }
     fn encode(msg: Message) -> Vec<u8> {
         match msg {
@@ -48,9 +43,29 @@ impl MessageEncoder {
     }
 }
 
-impl Parser<Message, Vec<u8>, Error> for MessageEncoder {
-    fn process_next_item(&mut self, item: Message) -> ParseStatus<Vec<u8>, Error> {
-        ParseStatus::Finished(Self::encode(item))
+impl Parser<Message, u8, Error> for MessageEncoder {
+    fn process_next_item(&mut self, item: Message) -> ParseStatus<u8, Error> {
+        let bytes = Self::encode(item);
+        if bytes.len() > 1 {
+            self.more = Some(bytes[1..].to_vec());
+            ParseStatus::Output(bytes[0])
+        } else {
+            ParseStatus::Output(bytes[0])
+        }
+    }
+    fn get_more(&mut self) -> Option<u8> {
+        let more = self.more.take();
+        if let Some(more) = more {
+            self.more_index += 1;
+            if self.more_index >= more.len() {
+                self.more_index = 0;
+                return None;
+            }
+            let byte = more[self.more_index];
+            self.more = Some(more);
+            return Some(byte);
+        }
+        return None;
     }
 }
 
@@ -71,26 +86,30 @@ impl Parser<u8, Message, Error> for MessageDecoder {
         match self.context.last() {
             None => {
                 self.context.push(item);
-                ParseStatus::Incomplete
+                ParseStatus::NeedsMore
             }
             Some(0) => match item {
-                0 => ParseStatus::Finished(Message::Noop),
+                0 => ParseStatus::Output(Message::Noop),
                 _ => {
                     self.context.push(item);
-                    ParseStatus::Incomplete
+                    ParseStatus::NeedsMore
                 }
             },
             Some(_) => match item {
                 0 => match String::from_utf8(self.context.clone()) {
-                    Ok(s) => ParseStatus::Finished(Message::Echo(s)),
+                    Ok(s) => ParseStatus::Output(Message::Echo(s)),
                     Err(e) => ParseStatus::Error(Error(e.to_string())),
                 },
                 _ => {
                     self.context.push(item);
-                    ParseStatus::Incomplete
+                    ParseStatus::NeedsMore
                 }
             },
         }
+    }
+
+    fn get_more(&mut self) -> Option<Message> {
+        None
     }
 }
 
@@ -101,11 +120,14 @@ impl Parser<std::io::Result<u8>, Message, Error> for MessageDecoder {
             Err(e) => ParseStatus::Error(e.into()),
         }
     }
+    fn get_more(&mut self) -> Option<Message> {
+        None
+    }
 }
 
 #[derive(Debug, PartialEq)]
 struct Error(String);
-    
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.0)
@@ -120,50 +142,132 @@ impl From<std::io::Error> for Error {
 
 impl std::error::Error for Error {}
 
+macro_rules! ok_or_log_err {
+    ($x: expr) => {
+        match $x {
+            Ok(a) => a,
+            Err(e) => {
+                log::error!("{e:?}");
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! async_lock {
+    ($x: ident, $y: block) => {{
+        let $x = $x.clone();
+        async move {
+            let mut $x = $x.lock().unwrap();
+            $y
+        }
+    }};
+
+    ($x: ident, $y: expr) => {{
+        let $x = $x.clone();
+        async move {
+            let mut $x = $x.lock().unwrap();
+            $y
+        }
+    }};
+}
+
+macro_rules! take_unless {
+    ($byte: ident, $($ns:literal),+) => {
+        future::ready(match $byte {
+            $(
+            Ok($ns) => false,
+            ),+
+            Ok(_) => true,
+            Err(e) => {
+                log::error!("{e:?}");
+                false
+            }
+        })
+    };
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let listen = "127.0.0.1:0".parse::<SocketAddr>()?;
     task::block_on(async {
         let listener = async_std::net::TcpListener::bind(listen).await?;
         let listen_addr = listener.local_addr()?;
-        let jh = task::spawn(async move {
-            listener
-                .incoming()
-                .for_each_concurrent(Some(3), |stream| async move {
-                    let stream = match stream {
-                        Ok(s) => s,
-                        Err(e) => {
-                            log::error!("{e:?}");
-                            return;
-                        }
-                    };
-                    let (reader, mut writer) = stream.split();
-                    reader
-                        .bytes()
-                        .parse_with(MessageDecoder::new()) // magic ✨
-                        .for_each(|req| async move {
-                            match req {
-                                Ok(req) => match writer.write(&MessageEncoder::encode(req)).await {
-                                    Ok(_) => {}
-                                    Err(e) => log::error!("{e:?}"),
-                                },
-                                Err(e) => log::error!("{e:?}"),
-                            }
+        let service_fut = listener
+            .incoming()
+            .take(1) // otherwise service keeps running
+            .for_each_concurrent(None, |stream| async move {
+                let stream = ok_or_log_err!(stream);
+                let peer_addr = ok_or_log_err!(stream.peer_addr());
+                log::debug!("new client: {}", peer_addr);
+                let (reader, mut writer) = stream.split();
+                let (tx, rx) = mpsc::channel(32);
+                let tx = Arc::new(Mutex::new(tx));
+                let reader_fut = reader
+                    .bytes()
+                    .inspect(|byte| {
+                        let byte = ok_or_log_err!(byte);
+                        log::debug!("service got byte: {byte}");
+                    })
+                    .take_while(|byte| take_unless!(byte, 3))
+                    .parse_with(MessageDecoder::new())
+                    .inspect(|req| {
+                        let req = ok_or_log_err!(req);
+                        log::debug!("service got req: {req:?}");
+                    })
+                    .for_each(|req| {
+                        async_lock!(tx, {
+                            let req = ok_or_log_err!(req);
+                            ok_or_log_err!(tx.send(req).await);
                         })
-                        .await
-                });
-            Result::<(), Error>::Ok(())
-        });
-        let client = std::net::TcpStream::connect(listen_addr)?;
-        stream::iter([Message::Noop, Message::Echo("test".into())])
-            .parse_with(MessageEncoder::new()) // magic ✨
-            .for_each(|bytes| async move {
-                if let Some(bytes) = take_ok_or_log_err(bytes) {
-                    client.write_all(&bytes);
+                    });
+                let writer_fut = async {
+                    let mut chunk_stream = rx
+                        .inspect(|req| {
+                            log::debug!("service sending req: {req:?}");
+                        })
+                        .parse_with(MessageEncoder::new())
+                        .take_while(|byte| future::ready(byte.is_ok()))
+                        .map(|byte| byte.unwrap())
+                        .ready_chunks(16);
+                    while let Some(bytes) = chunk_stream.next().await {
+                        log::debug!("service sending bytes: {bytes:?}");
+                        ok_or_log_err!(writer.write_all(&bytes).await);
+                    }
+                };
+                futures::future::join(reader_fut, writer_fut).await;
+                log::debug!("done handling client");
+            });
+        let mut client = async_std::net::TcpStream::connect(listen_addr).await?;
+        let client_fut = async move {
+            let (tx, mut rx) = mpsc::channel(32);
+            let tx = Arc::new(Mutex::new(tx));
+            let msgs = [Message::Noop, Message::Echo("test".into())];
+            let encode_fut = stream::iter(msgs)
+                .parse_with(MessageEncoder::new())
+                .take_while(|byte| future::ready(byte.is_ok()))
+                .map(|byte| byte.unwrap())
+                .ready_chunks(16)
+                .for_each(|bytes| async_lock!(tx, ok_or_log_err!(tx.send(bytes).await)))
+                .then(|_| async { log::debug!("done encoding messages") });
+            let send_fut = async {
+                if let Some(bytes) = rx.next().await {
+                    log::debug!("sending bytes: {bytes:?}");
+                    ok_or_log_err!(client.write_all(&bytes).await);
                 }
-            })
-            .await;
-        jh.cancel().await;
+                log::debug!("done sending bytes");
+            };
+            futures::future::join(encode_fut, send_fut).await;
+            log::debug!("receiving bytes on client");
+            client
+                .bytes()
+                .for_each(|byte| async move {
+                    let byte = ok_or_log_err!(byte);
+                    log::debug!("client got byte: {byte}");
+                })
+                .await;
+        };
+        futures::future::join(service_fut, client_fut).await;
         Result::<(), Error>::Ok(())
     })?;
     Ok(())
--- src/stream_parser.rs
+++ src/stream_parser.rs
@@ -2,17 +2,19 @@ use std::{io, marker::PhantomData, pin::Pin, sync::Mutex};
 
 use async_std::{
     io::Bytes,
+    stream::TakeWhile,
     task::{Context, Poll},
 };
-use futures::{channel::mpsc, stream, AsyncRead, Stream, StreamExt};
+use futures::{channel::mpsc, future::Ready, stream, AsyncRead, Future, Stream, StreamExt};
 
 pub trait Parser<I, O, E> {
     fn process_next_item(&mut self, item: I) -> ParseStatus<O, E>;
+    fn get_more(&mut self) -> Option<O>;
 }
 
 pub enum ParseStatus<T, E> {
-    Finished(T),
-    Incomplete,
+    Output(T),
+    NeedsMore,
     Error(E),
 }
 
@@ -29,19 +31,34 @@ where
     }
 }
 
-impl<I, O, E, P> ParseWith<I, O, E, P> for mpsc::Receiver<I> where P: Parser<I, O, E> {}
+// impl<I, O, E, P> ParseWith<I, O, E, P> for mpsc::Receiver<I> where P: Parser<I, O, E> {}
+//
+// impl<I, O, E, P, It> ParseWith<I, O, E, P> for stream::Iter<It>
+// where
+//     P: Parser<I, O, E>,
+//     It: Iterator<Item = I>,
+// {
+// }
+//
+// impl<O, E, P, R> ParseWith<io::Result<u8>, O, E, P> for Bytes<R>
+// where
+//     P: Parser<io::Result<u8>, O, E>,
+//     R: AsyncRead + Unpin,
+// {
+// }
+//
+// impl<I, O, E, P, S, Pred> ParseWith<I, O, E, P> for TakeWhile<S, Pred>
+// where
+//     S: ParseWith<I, O, E, P>,
+//     P: Parser<I, O, E>,
+//     Pred: FnMut(&I) -> bool,
+// {
+// }
 
-impl<I, O, E, P, It> ParseWith<I, O, E, P> for stream::Iter<It>
+impl<I, O, E, P, S> ParseWith<I, O, E, P> for S
 where
+    S: Stream<Item = I>,
     P: Parser<I, O, E>,
-    It: Iterator<Item = I>,
-{
-}
-
-impl<O, E, P, S> ParseWith<io::Result<u8>, O, E, P> for Bytes<S>
-where
-    P: Parser<io::Result<u8>, O, E>,
-    S: AsyncRead + Unpin,
 {
 }
 
@@ -62,18 +79,20 @@ where
 {
     type Item = Result<O, E>;
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if let Some(o) = self.parser.lock().unwrap().get_more() {
+            return Poll::Ready(Some(Ok(o)));
+        }
         loop {
             let next_item: I = match self.inner.lock().unwrap().poll_next_unpin(cx) {
                 Poll::Ready(Some(t)) => t,
                 Poll::Ready(None) => break Poll::Ready(None),
                 Poll::Pending => break Poll::Pending,
             };
+            use ParseStatus::*;
             match self.parser.lock().unwrap().process_next_item(next_item) {
-                ParseStatus::Finished(o) => {
-                    break Poll::Ready(Some(Ok(o)));
-                }
-                ParseStatus::Incomplete => {}
-                ParseStatus::Error(e) => break Poll::Ready(Some(Err(e))),
+                Output(o) => break Poll::Ready(Some(Ok(o))),
+                NeedsMore => {}
+                Error(e) => break Poll::Ready(Some(Err(e))),
             }
         }
     }
